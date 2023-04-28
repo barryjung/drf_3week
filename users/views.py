42bb437e65b46edada3f0a48615309018a630e13
barryjung <wjdgusrbs5@gmail.com>
2023-04-28 16:03:45 +0900

    Update 4weeks

--- users/views.py
+++ users/views.py
@@ -1,11 +1,13 @@
 from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
 from rest_framework import status
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
 )
-from users.serializers import CustomTokenObtainPairSerializer, UserSerializer
+from users.models import User
+from users.serializers import CustomTokenObtainPairSerializer, UserSerializer, UserProfileSerializer
 
 
 class UserView(APIView):
@@ -18,6 +20,18 @@ class UserView(APIView):
             return Response({"message":f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
         
 
+class FollowView(APIView):
+    def post(self, request, user_id):
+        you = get_object_or_404(User, id=user_id)
+        me = request.user
+        if me in you.followers.all():
+            you.followers.remove(me)
+            return Response("unfollow했습니다.", status=status.HTTP_200_OK)
+        else:
+            you.followers.add(me)
+            return Response("follow했습니다.", status=status.HTTP_200_OK)
+        
+        
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 
@@ -25,4 +39,14 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 class mockView(APIView):
     permission_classes = [permissions.IsAuthenticated]
     def get(self, request):
+        print(request.user)
+        user=request.user
+        user.is_admin = True
+        user.save()
         return Response("get 요청")
+    
+class ProfileView(APIView):
+    def get(self, request, user_id):
+        user = get_object_or_404(User, id=user_id)
+        serializer = UserProfileSerializer(user)
+        return Response(serializer.data)
